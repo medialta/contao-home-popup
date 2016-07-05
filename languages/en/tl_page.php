0fc5446393ecb89eea2bc72be448a90fe4aec507
Lucas GEHIN <lucas@medialta.com>
2016-07-05 09:24:31 +0200

    add english translation

---
+++ languages/en/tl_page.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @package   Contao
+ * @author    Medialta <contact@medialta.com>
+ * @copyright Medialta 2016
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_page']['popup_enable']   = array('Active pop-up', 'Displays a pop up on the home page.');
+$GLOBALS['TL_LANG']['tl_page']['popup_title']  = array('Pop-up title', '');
+$GLOBALS['TL_LANG']['tl_page']['popup_content']   = array('Pop-up content', '');
+$GLOBALS['TL_LANG']['tl_page']['popup_button']   = array('Button label', '(e.g. Close).');
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_page']['popup_legend'] = 'Pop-up';
